45b3472a21386319c2fa0728d651a824fd071742
ODS <idsorlyk@gmail.com>
2025-12-24 11:31:25 +0100

    feat: add Axios API client with dynamic base URL and credentials.

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const baseURL = typeof window === 'undefined' 
+  ? (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000') + "/api"
+  : "/api";
+
 export const nextServer = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api", // додано /api
+  baseURL: baseURL,
   withCredentials: true,
 });
